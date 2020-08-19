f485609446a996c1b0befcd41b3ab6d1dd1026cf
Poulami Sarkar <poulamisarkar@SSMacBookPro2018.local>
2020-08-19 10:05:04 +0530

    Latest changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
 
 var data;
 var result;
+var toggle='2D';
+var range='1>rho>0';
+var table_body='';
 
 $('ul a.nav-link').on('click', function(e) {
     $('.arrow-down').removeClass('active-arrow');
@@ -84,9 +87,65 @@ $('.search-box').change(function(e){
 			break;
 	var rank = result[i]['Ranking']
 	$('.univeristy-name').html('#'+rank+' ' +uni);
+	var table='';
 	var icr = result[i]['NLIQ']
-	$('.icr').html('ICR '+icr);	
 	var nliq = result[i]['ICR']
-	$('.nliq').html('NLIQ '+nliq);
-
+	table += '<tr><td>ICR<td/><td>'+icr+'<td/></tr>';
+	table += '<tr><td>NLIQ<td/><td>'+nliq+'<td/></tr>';
+	$('.uni-data').html(table);
+	
+	// Get specific university data
+	table_body='';
+	for( key in result[i]){
+		table_body+='<tr><th scope="row">'+key+'</th>';
+		table_body+='<td>'+result[i][key]+'</td></tr>';
+	}
+	$('.complete-uni-data').html(table_body);
+	console.log(table_body);
 });
+
+
+/*
+* Change the graph
+*/
+
+$("a").click(function(){
+	// Change src attribute of image
+	$('#collapseContent div h4').html('Pyswarm learning curve');
+	range = $(this).attr('href')
+	var tab_id =range + ' img';
+
+	if(range == '#d21')
+		range = '1>rho>0';
+	else if(range == '#d22')
+		range = '0.1>rho>0.001';
+	else if(range == '#d23')
+		range = '0.99>rho>0.9';	
+	else if(range == '#d24')
+		range = '0.9>rho>0.101';
+	else if(range == '#d25')
+		range = 'rho>1';
+	
+	var uni = $('.search-box').children("option:selected").val();
+	$(tab_id).attr('src','../assets/viz/'+toggle+'/'+range+'/cost_'+toggle+'_'+uni+'.png');
+	console.log(tab_id);
+	console.log('../assets/viz/'+toggle+'/'+range+'/cost_'+toggle+'_'+uni);
+	//$(this).attr("src", "./public/assets/cost_history/cost.png");
+});
+
+
+$('.toggle-slider').change(function(){
+	status= $(this).prop("checked"); 
+	if(status == "false")
+		toggle = '2D';
+	else
+		toggle ='3D';
+	console.log(toggle);
+	var uni = $('.search-box').children("option:selected").val();
+	$(range + ' img').attr('src','../assets/viz/'+toggle+'/'+range+'/cost_'+toggle+'_'+uni+'.png');
+	console.log('../assets/viz/'+toggle+'/'+range+'/cost_'+toggle+'_'+uni+'.png')
+})
+
+function get_uni_dat(uni){
+
+}
